7becc77e90ba1eb17e44f2d0fd4156f95644041c
Kostya <example@gmail.com>
2024-03-09 16:13:29 +0200

    firstcommit

---
+++ JavaScript/script.js
@@ -0,0 +1,33 @@
+// const storeName = "Gucci";
+// const storeDescription = {
+//   budget: 10000,
+//   employees: ["Ivan", "Robert", "Mike"],
+//   products: { price: 2000 },
+//   open: "open",
+// };
+// console.log(storeDescription);
+// alert("hello");
+// const result = confirm('Как твои дела?')
+// console.log(result)
+// const answer = +prompt('Вам есть 18?', 19)
+// console.log(answer+5)
+// const answers = []
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько тебе лет?', '')
+// document.write(typeof(answers));
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`)
+// console.log('arr'+ +"5")
+// let incr = 10,
+//    decr = 10;
+
+// console.log(incr++)
+// console.log(decr--)
+console.log(98%2);
+console.log(6*8 == 48)
+const isChecked = true,
+     isClose = false;
+
+     console.log(isChecked || isClose)
+console.log(2 + 2 * 2 !== '6')
